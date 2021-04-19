b05d227acd75e611469fe78f017b3a4915502ad9
enriquegarcia96 <enriquegarcia259@yahoo.es>
2021-04-19 13:32:30 -0600

    se agrego el navbar y la validacion de que tiene que estar registrado el usuario

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import Chat from './components/Chat';
+import Navbar from './components/Navbar';
+
+import { ChatContext } from './context/ChatProvider'
+
 
 const App = () => {
-    return (
+
+    const {usuario} =  React.useContext(ChatContext);
+
+    return usuario != null ? (
         <div>
-            chat
+            <Navbar />
+            {
+                usuario.estado ? (
+                    <Chat />
+                ) : (
+                    <>
+                        <div className='lead text-center mt-5'>Debes de Iniciar Sesion</div>
+                        <div className='lead text-center mt-5'>Enrique S. García</div>
+                    </>
+                    
+                )
+            }
         </div>
+    ): (
+        <div>Cargando Aplicacion...</div>
     )
 }
 
